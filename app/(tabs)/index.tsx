3cee35df88066ff7c22cf59342c8c86e41276932
Manasvi Verma <127782338+vermamanasvi@users.noreply.github.com>
2025-09-06 01:14:53 +0530

    Home screen UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className = 'flex-1 justify-center items-center'>
-      <Text className="text-5xl text-dark-200 font-bold"> Welcome!</Text>
+    <View className="flex-1 bg-primary">
+      <Image source = {images.bg} className = "absolute w-fullz-0" />
+      <ScrollView className = 'flex-1 px-5' showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: "100%", paddingBottom: 10 }}>
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      </ScrollView>
     </View>
-
   );
 }
